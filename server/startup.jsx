c4e55ffdbe4181c01e639a966030fe9f3012d837
Bjorn Tegelund <b.tegelund@gmail.com>
2015-10-29 23:32:59 +0900

    Removed a weird symbol from the json file and made some small fixed

--- server/startup.jsx
+++ server/startup.jsx
@@ -1,9 +1,10 @@
 if(Meteor.isServer) {
   Meteor.startup(function() {
-    
+
     // Add listings to the database if there aren't any
     if(Listings.find().count() === 0) {
       let data = JSON.parse(Assets.getText('job_listings.json'));
+      console.log(data);
       data.forEach((l) => {
         Listings.insert(l);
       });
